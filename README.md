4d0414c9a63b3df31012d9c5c427874b8d60171a
Lino Figueroa <lino.figueroa@softwareone.com>
2024-02-02 19:58:36 +0100

    First upload. Quick and dirty functionality

--- README.md
+++ README.md
@@ -1,2 +1,96 @@
 # tcp-dummy-services
+
 Dummy services to test connectivity on cloud providers
+
+## Technology Stack:
+
+- Python
+- Docker
+- Pytest (\*)
+- PostgreSql/Mysql
+
+## Development environment
+
+### Requirements:
+
+- Docker CE (Linux) or Docker Desktop (MacOS, Windows).
+- Python >= 3.12 (Pyenv, best option)
+- Poetry as dependency manager
+
+### Activate development environment
+
+```
+poetry install
+```
+
+This will create a new virtual environment (if it does not exists) and will install all the dependencies.
+
+To activate the virtual environment use:
+
+```
+poetry shell
+```
+
+### Add/remove dependencies
+
+```
+poetry add PIP_PACKAGE [-G group.name]
+```
+
+Add dependency to the given group. If not specified will be added to the default group.
+
+```
+poetry remove PIP_PACKAGE [-G group.name]
+```
+
+Remove dependency from the given group
+
+### Run project from command line
+
+```
+
+poetry run python .\src\aws_tcp_dummy_services\ 7000
+
+```
+
+### Debug project from VS Code
+
+First create a .env file in the root folder or copy the existing .env.example and set the required variables
+
+Then use the Launch option from Visual Studio Code
+
+## Tests
+
+### Debug From VS Code
+
+Get the path of the virtual environment created by poetry:
+
+```bash
+poetry env info -p
+```
+
+Set in visual studio code the default interpreter to the virtual environment created by poetry.(SHIT+CTRL+P Select interpreter)
+
+Launch "Pytest launch" from the run/debug tab.
+
+You can set breakpoints and inspections
+
+## Docker build and run
+
+### Build
+
+From root directory execute:
+
+```bash
+docker build -f ./docker/Dockerfile -t tcp-dummy-services:latest .
+```
+
+### Run
+
+From root directory execute:
+
+```bash
+docker run -d -p 7000:7000 --name tcp-dummy-services tcp-dummy-services:latest
+```
+
+Do not use "localhost" as LOCAL_SERVER_IP, use your local IP address instead. Docker container will not be able to connect to your local database otherwise.
---
+++ src/tcp_dummy_services/__main__.py
@@ -0,0 +1,89 @@
+import asyncio
+import argparse
+
+import logging
+import sys
+
+# Configure logging
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    handlers=[logging.StreamHandler(sys.stdout)],
+)
+logger = logging.getLogger()
+
+
+async def handle_client(reader, writer):
+
+    addr = writer.get_extra_info("peername")
+
+    try:
+        while True:
+            data = await reader.readline()
+            if not data:
+                break
+
+            message = data.decode().strip()
+            if message == "END":
+                break
+
+            logger.info(f"Received message from {addr}: {message}")
+
+            writer.write(data)
+            await writer.drain()
+    except Exception as e:
+        logger.error(f"An error occurred: {e}")
+    finally:
+        logger.info(f"Closing the connection on {addr}")
+        writer.close()
+        await writer.wait_closed()
+
+
+async def main(host, start_port, end_port):
+    for port in range(start_port, end_port + 1):
+        server = await asyncio.start_server(handle_client, host, port)
+
+        addr = server.sockets[0].getsockname()
+        logger.info(f"Serving on {addr}")
+
+        async with server:
+            await server.serve_forever()
+
+
+if __name__ == "__main__":
+
+    # TODO: Change argument parsing to another function
+    parser = argparse.ArgumentParser(description="Asyncio Server")
+    parser.add_argument(
+        "ports",
+        help="The port or range of ports to start the server on. Format: start-end or port",
+    )
+    parser.add_argument(
+        "--host",
+        default="0.0.0.0",
+        help="The host to start the server on. Default is 0.0.0.0",
+    )
+
+    args = parser.parse_args()
+
+    # Check if ports argument is a range or a single port
+    if "-" in args.ports:
+        start_port, end_port = map(int, args.ports.split("-"))
+    else:
+        start_port = end_port = int(args.ports)
+
+    try:
+        asyncio.run(main(args.host, start_port, end_port))
+    except KeyboardInterrupt:
+        # Get all running tasks:
+        tasks = asyncio.all_tasks()
+        for task in tasks:
+            task.cancel()
+        # Wait until all tasks are cancelled:
+        asyncio.get_event_loop().run_until_complete(
+            asyncio.gather(*tasks, return_exceptions=True)
+        )
+        logger.info("Server shut down.")
+
+    except Exception as e:
+        logger.error(f"An error occurred: {e}")
